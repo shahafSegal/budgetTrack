41e9180c8bcef24b8e6a4cc02902fa4b88d76451
shahafSegal <goigadol360@gmail.com>
2024-01-18 15:17:04 +0200

    added react-bootstrap

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,33 +1,40 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Nav,Container, Navbar } from "react-bootstrap";
 import { NavLink} from 'react-router-dom'
 import UserDropDown from "./userDropDown";
 function NavBar(props){
     const userObj=props.usrObj;
     const activeClass=({ isActive }) => ({
-        color: isActive ? '#fff' : '#545e6f',
-        background: isActive ? '#7600dc' : '#f0f0f0',
+        color: isActive ? '#C0C0C0' : '#545e6f',
+        background: isActive ? '#7600dc' : '#C0C0C0',
       })
 
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-        <span className="navbar-brand btn btn-dark active">budgetApp</span>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <NavLink className="nav-item nav-link" style={activeClass} to="/">Home</NavLink>
-            <NavLink className="nav-item nav-link" style={activeClass} to="/budget">Budget Tracker</NavLink>
-          </div>
-  
-        </div>
-        <ul className="navbar-nav d-flex flex-row me-1">
-          {userObj.loggedIn?<UserDropDown username={userObj.username} logOut={props.logOut}/>
-          :<NavLink className="nav-item nav-link me-3 me-lg-0" style={activeClass} to="/register">Register</NavLink>
-          }
-        </ul>
-      </nav>
+      <Navbar bg="primary" expand="lg" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>BudgetApp</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/" style={activeClass}>
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/budget" style={activeClass}>
+                Budget
+              </Nav.Link>
+            </Nav>
+            <Nav>
+            {userObj.loggedIn?<UserDropDown username={userObj.username} logOut={props.logOut}/>
+            :<Nav.Link as={NavLink} style={activeClass} to="/register">Register</Nav.Link>
+            }
+            </Nav>
+            
+
+          </Navbar.Collapse>
+          
+          
+        </Container>
+    </Navbar>
     )
 }
 export default NavBar;
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbWE29UhTtCT3fpiScruVNDVR0zmgINMA",
+  authDomain: "budgetapp-51feb.firebaseapp.com",
+  projectId: "budgetapp-51feb",
+  storageBucket: "budgetapp-51feb.appspot.com",
+  messagingSenderId: "838258813823",
+  appId: "1:838258813823:web:617b39290cbb424b9c884a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
